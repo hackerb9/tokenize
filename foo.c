a1e2cb4521ce73ae71fc2eac8c6c52d2a7d6a0a4
b9 <hackerb9@member.fsf.org>
2024-07-15 22:37:43 -0700

    blah

--- foo.c
+++ foo.c
@@ -2,15 +2,17 @@
 main() {
   FILE *fp;
   fp = stdout;
-  if ( fp = fopen("text", "w") ) {
+  fprintf(fp, "foo\nbar\n");
+  fprintf(fp, "foo\012bar\012");
+  if ( fp = freopen(NULL, "wb", stdout) ) {
+    printf("foo\nbar\n");
+    printf("foo\012bar\012");
+  }
+  if ( fp = freopen("text", "w", fp) ) {
     fprintf(fp, "foo\nbar\n");
     fprintf(fp, "foo\012bar\012");
   }
   if ( fp = freopen("binary", "wb", fp) ) {
-    fprintf(fp, "foo\nbar\n");
-    fprintf(fp, "foo\012bar\012");
-  }
-  if ( fp = freopen("binaryplus", "wb+", fp) ) {
     fprintf(fp, "foo\nbar\n");
     fprintf(fp, "foo\012bar\012");
   }
