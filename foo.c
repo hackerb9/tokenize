de11ef339b3fa7e430eb7013b5c38cf593471078
b9 <hackerb9@member.fsf.org>
2024-07-15 18:36:48 -0700

    does index work?

--- foo.c
+++ foo.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 main() {
-  printf("foo\012bar\012");
+  FILE *fp;
+  if ( fp = fopen("newline", "w") ) {
+    fprintf(fp, "foo\nbar\n");
+    fprintf(fp, "foo\012bar\012");
+    fprintf(fp, "foo\033\012bar\012");
+  }
 }
