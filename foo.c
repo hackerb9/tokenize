d47de21b1184433fcb79f33192e52c0871378c99
b9 <hackerb9@member.fsf.org>
2024-07-15 22:26:57 -0700

    blah

--- foo.c
+++ foo.c
@@ -4,6 +4,6 @@ main() {
   if ( fp = fopen("newline", "w") ) {
     fprintf(fp, "foo\nbar\n");
     fprintf(fp, "foo\012bar\012");
-    fprintf(fp, "foo\033\012bar\012");
+    fprintf(fp, "foo\eEbar\eE");
   }
 }
